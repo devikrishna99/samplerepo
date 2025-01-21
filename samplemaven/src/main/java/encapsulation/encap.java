228ef9767c2ba33da13070c0861fd028a8ff4c5c
devik <devik@DESKTOP-9DL12AG>
2025-01-21 14:48:07 +0530

    encapcommit

---
+++ samplemaven/src/main/java/encapsulation/encap.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class encap {
+private String name;
+private int age;
+	public void setter(String name ,int age) {
+		this.name = name;
+		this.age=age;
+	}
+	public void getter() {
+		System.out.println(name+ " "+age);
+	}
+}
---
+++ samplemaven/src/main/java/encapsulation/encap2.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class encap2 {
+	
+	public static void main(String[] args) {
+		encap obj = new encap();
+		obj.setter("Devi", 25);
+		obj.getter();
+	}
+
+}
